fc6a90a8f2a711056f136da0122bf334e23029b7
Juan Carlos Coral <govindasd@gmail.com>
2017-10-15 16:32:00 -0200

    Montando o ambiente para desenvolvimento
    Configurando NPM, Grunt e criando arquivos e pastas para começar a desenvolver o widget.

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		coffee: {
+			compile: {
+		    files: {
+		      'scripts/app.js': ['_source/scripts/app.coffee'] // compile and concat into single file 
+		    }
+		  },
+		  compileJoined: {
+		    options: {
+		      join: true
+		    },
+		    files: {
+		      'scripts/app.js': ['_source/scripts/app.coffee'] // compile and concat into single file 
+		    }
+		  }
+		},
+		sass: {                             
+	    dist: {                         
+	      files: {                         
+	        'styles/app.css': '_source/styles/app.scss'       // 'destination': 'source'
+	      }
+	    }
+	  },
+	  watch: {
+		  scripts: {
+		    files: '_source/**/*.coffee',
+		    tasks: ['coffee']
+		  },
+		  sass:    {
+		  	files: '_source/**/*.scss',
+		  	tasks: ['sass']
+		  }
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['sass', 'coffee']);
+};
