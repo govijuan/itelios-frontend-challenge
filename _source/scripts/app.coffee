2247dfe14c4314a1bfbb63d8749854a6867599b6
Juan Carlos Coral <govindasd@gmail.com>
2017-10-17 09:14:41 -0200

    Montando slider Incompleto
    Importada a biblioteca 'lory' para slider e configurando os slides, sem funcionamento correto ainda

--- _source/scripts/app.coffee
+++ _source/scripts/app.coffee
@@ -1,5 +1,6 @@
 dataXhttp = null
 dataArray = new Object
+### Função que Carrega o JSON ###
 loadJSON = (callback)->
 	if window.XMLHttpRequest
 		dataXhttp = new XMLHttpRequest()
@@ -11,15 +12,33 @@ loadJSON = (callback)->
 			callback(dataXhttp.responseText)
 			seenItemObj = dataArray.item
 			recommItemsListObj = dataArray.recommendation
-			console.log(dataArray)
-			console.log(seenItemObj)
-			console.log(recommItemsListObj)
+			console.log dataArray
+			printRecommList(recommItemsListObj)
 	dataXhttp.open('GET', "products.json", true)
 	dataXhttp.send()
+### Função que Inicia procesos do javascript ###
 init = ()->
 	loadJSON( (response)->
 		dataArray = JSON.parse(response)[0].data
 	)
+### Imprime os Items recomendados na div correspondente ###
+printRecommList = (objListArray)->
+	recomendedItemList = ''
+	for recomItem in objListArray
+		paymentConditionsStr = recomItem.productInfo.paymentConditions
+		ouAteStr = paymentConditionsStr.slice(0, 6)
+		semJurosStr = paymentConditionsStr.slice(-10)
+		paymentConditionsStrArr = paymentConditionsStr.split(' ')
+		recomItemOutput =  '<li class="recom-item-wrap js_slide">
+													<div class"item-img-div" businessId="' + recomItem.businessId + '"><img src="' + recomItem.imageName + '" ></div>
+													<div class="item-description">' + recomItem.name + '</div>
+													<div class="">Por <span class="item-price">' + recomItem.price + '</span></div>
+													<div class="pay-conditions">' + ouAteStr + ' <span class="pay-cond-fees">' + paymentConditionsStrArr[2] + ' ' + paymentConditionsStrArr[3] + ' ' + paymentConditionsStrArr[4] + ' ' + paymentConditionsStrArr[5] + '</span>' + semJurosStr + '</div>
+													<button class="ad-to-cart-btn">adicionar ao carrinho <img src="images/ic_add_shopping_cart_black_24px.svg" class="cart-icon-img"></button>						
+												</li>'
+		recomendedItemList += recomItemOutput
+	document.querySelector(".recom-item-list").innerHTML = recomendedItemList
+### Iniciando processos de JavaScript ###
 init()
-##console.log('Hurray')
+
 
--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,12 @@
 (function() {
-  var dataArray, dataXhttp, init, loadJSON;
+  var dataArray, dataXhttp, init, loadJSON, printRecommList;
 
   dataXhttp = null;
 
   dataArray = new Object;
+
+
+  /* Função que Carrega o JSON */
 
   loadJSON = function(callback) {
     if (window.XMLHttpRequest) {
@@ -18,19 +21,42 @@
         seenItemObj = dataArray.item;
         recommItemsListObj = dataArray.recommendation;
         console.log(dataArray);
-        console.log(seenItemObj);
-        return console.log(recommItemsListObj);
+        return printRecommList(recommItemsListObj);
       }
     };
     dataXhttp.open('GET', "products.json", true);
     return dataXhttp.send();
   };
 
+
+  /* Função que Inicia procesos do javascript */
+
   init = function() {
     return loadJSON(function(response) {
       return dataArray = JSON.parse(response)[0].data;
     });
   };
+
+
+  /* Imprime os Items recomendados na div correspondente */
+
+  printRecommList = function(objListArray) {
+    var i, len, ouAteStr, paymentConditionsStr, paymentConditionsStrArr, recomItem, recomItemOutput, recomendedItemList, semJurosStr;
+    recomendedItemList = '';
+    for (i = 0, len = objListArray.length; i < len; i++) {
+      recomItem = objListArray[i];
+      paymentConditionsStr = recomItem.productInfo.paymentConditions;
+      ouAteStr = paymentConditionsStr.slice(0, 6);
+      semJurosStr = paymentConditionsStr.slice(-10);
+      paymentConditionsStrArr = paymentConditionsStr.split(' ');
+      recomItemOutput = '<li class="recom-item-wrap js_slide"> <div class"item-img-div" businessId="' + recomItem.businessId + '"><img src="' + recomItem.imageName + '" ></div> <div class="item-description">' + recomItem.name + '</div> <div class="">Por <span class="item-price">' + recomItem.price + '</span></div> <div class="pay-conditions">' + ouAteStr + ' <span class="pay-cond-fees">' + paymentConditionsStrArr[2] + ' ' + paymentConditionsStrArr[3] + ' ' + paymentConditionsStrArr[4] + ' ' + paymentConditionsStrArr[5] + '</span>' + semJurosStr + '</div> <button class="ad-to-cart-btn">adicionar ao carrinho <img src="images/ic_add_shopping_cart_black_24px.svg" class="cart-icon-img"></button> </li>';
+      recomendedItemList += recomItemOutput;
+    }
+    return document.querySelector(".recom-item-list").innerHTML = recomendedItemList;
+  };
+
+
+  /* Iniciando processos de JavaScript */
 
   init();
 
